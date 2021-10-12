5c3d1e6da57bb2983bf5b2a947979cd0c72c5749
tahmid akter <tahmidakter56@gmail.com>
2021-10-12 06:56:32 +0600

    day 10 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial2/models/catalog.dart';
 import 'package:tutorial2/widgets/drawer.dart';
+import 'package:tutorial2/widgets/item_widgets.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -7,13 +9,21 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String name = "Tahmid";
-
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Welcome $name"),
       ),
-      body: Center(
-        child: Text('Hello $name'),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: MyDrawer(),
     );
